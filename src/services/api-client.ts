01782eff8339761e6ef04928703753f1c1ab445c
Zach <user@Users-MBP.lan>
2023-07-11 16:05:28 -0700

    first commit

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "4409cbcfb4fc4d6d8534982c08647ee2",
   },
 });
